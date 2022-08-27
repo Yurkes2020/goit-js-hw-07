d4e8566676bb2403ad2a71c35a7a9766603986db
Yurkes2020 <yurkes2020@gmail.com>
2022-08-27 18:38:57 +0300

    final

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const createGalleryItem = ({ preview, original, description }) => {
+  return `
+    <div class="gallery__item">
+        <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+    </div>`;
+};
+
+const createGalleryMarkup = () => {
+  const galleryMarkup = galleryItems.map(createGalleryItem).join('');
+  galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+};
+
+createGalleryMarkup();
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
